30b4c5ce84c6d8c1acc053a9a1863e85d5da78cd
Mayappa <mayappapujari561999@gmail.com>
2024-07-12 15:30:12 +0530

    added new function

--- src/script/script.js
+++ src/script/script.js
@@ -1,8 +1,8 @@
 // src/script/script.js
 
 async function getWeather() {
-const apiKey = import.meta.env.VITE_API_KEY;
-const city = document.getElementById("city-input").value;
+  const apiKey = import.meta.env.VITE_API_KEY;
+  const city = document.getElementById("city-input").value;
 
   if (!city) {
     alert("Please enter a city name");
@@ -33,6 +33,9 @@ function displayWeather(data) {
     <p>Humidity: ${data.main.humidity}%</p>
     <p>Wind Speed: ${data.wind.speed} m/s</p>
   `;
+  // summer background   "https://media.istockphoto.com/id/1226628621/photo/summer-background-orange-sky-with-clouds-and-bright-sun.webp?b=1&s=170667a&w=0&k=20&c=Zrx6fhLi6mYYJylYQsbYr09ozyNsgAYfNolc_pUgmMY="
+  //   rainy background   "https://images.unsplash.com/photo-1534274988757-a28bf1a57c17?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cmFpbnxlbnwwfHwwfHx8MA%3D%3D"
+  //   winter background   "https://images.unsplash.com/photo-1517166357932-d20495eeffd7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8c25vd2luZ3xlbnwwfHwwfHx8MA%3D%3D"
 }
 
 window.getWeather = getWeather;
