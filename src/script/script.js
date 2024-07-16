77f027570f7a278390c26f37ee5133c11b636c07
Mayappa <mayappapujari561999@gmail.com>
2024-07-16 19:24:55 +0530

    removed background image

--- src/script/script.js
+++ src/script/script.js
@@ -1,5 +1,4 @@
 async function getWeather() {
-  
   const apiKey = import.meta.env.VITE_API_KEY;
   const city = document.getElementById("city-input").value;
   const result = document.getElementsByClassName("weather-result")[0];
@@ -56,25 +55,6 @@ function displayWeather(data) {
     <p>Humidity: ${data.main.humidity}%</p>
     <p>Wind Speed: ${data.wind.speed} m/s</p>
   `;
-
-  let backgroundUrl;
-
-  if (temperatureCelsius >= 35) {
-    // Summer
-    backgroundUrl =
-      "https://media.istockphoto.com/id/1226628621/photo/summer-background-orange-sky-with-clouds-and-bright-sun.webp?b=1&s=170667a&w=0&k=20&c=Zrx6fhLi6mYYJylYQsbYr09ozyNsgAYfNolc_pUgmMY=";
-  } else if (temperatureCelsius < 35 && temperatureCelsius >= 10) {
-    // Rainy
-    backgroundUrl =
-      "https://images.unsplash.com/photo-1534274988757-a28bf1a57c17?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cmFpbnxlbnwwfHwwfHx8MA%3D%3D";
-  } else {
-    // Winter
-    backgroundUrl =
-      "https://images.unsplash.com/photo-1517166357932-d20495eeffd7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8c25vd2luZ3xlbnwwfHwwfHx8MA%3D%3D";
-  }
-
-  console.log(`Selected background URL: ${backgroundUrl}`);
-  document.body.style.backgroundImage = `url(${backgroundUrl})`;
 }
 
 window.getWeather = getWeather;
